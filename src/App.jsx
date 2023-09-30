c4b6f71c3df91c67632e9158f06670393b527e34
Dericki <derick.araujoo@gmail.com>
2023-09-30 17:01:04 -0300

    Criando login com Autenticação

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 import { Outlet } from "react-router-dom";
+import Footer from "./components/Footer";
+import Nav from "./components/Nav";
 
 export default function App()  {
   return (
     <div>
       <h1>Olá Mundo!</h1>
-
+        <Nav />
         <Outlet/>
-
+        <Footer />
     </div>
   )
 }
